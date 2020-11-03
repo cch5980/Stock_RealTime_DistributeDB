02ea8132c2215e210bf8dc386e72e19d16ee78aa
cch5980 <cch5980@gmail.com>
2020-11-03 18:57:47 +0900

    Stock_RealTimeData_winform

--- 실시간데이터수집/실시간데이터수집/Form1.Designer.cs
+++ 실시간데이터수집/실시간데이터수집/Form1.Designer.cs
@@ -30,27 +30,162 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.StockSearchTextBox = new System.Windows.Forms.TextBox();
+            this.StockSearchButton = new System.Windows.Forms.Button();
+            this.StockListDataGridView = new System.Windows.Forms.DataGridView();
+            this.종목조회_종목코드 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.종목조회_종목명 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StockCheDateDataGridView = new System.Windows.Forms.DataGridView();
+            this.csvOutputButton = new System.Windows.Forms.Button();
+            this.che_date_check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.che_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StockListDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StockCheDateDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // axKHOpenAPI1
             // 
             this.axKHOpenAPI1.Enabled = true;
-            this.axKHOpenAPI1.Location = new System.Drawing.Point(639, 364);
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(769, 474);
             this.axKHOpenAPI1.Name = "axKHOpenAPI1";
             this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
-            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(74, 27);
             this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.StockSearchTextBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.StockSearchButton, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(66, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(229, 39);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // StockSearchTextBox
+            // 
+            this.StockSearchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StockSearchTextBox.Location = new System.Drawing.Point(3, 3);
+            this.StockSearchTextBox.Name = "StockSearchTextBox";
+            this.StockSearchTextBox.Size = new System.Drawing.Size(108, 25);
+            this.StockSearchTextBox.TabIndex = 0;
+            // 
+            // StockSearchButton
+            // 
+            this.StockSearchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StockSearchButton.Location = new System.Drawing.Point(117, 3);
+            this.StockSearchButton.Name = "StockSearchButton";
+            this.StockSearchButton.Size = new System.Drawing.Size(109, 33);
+            this.StockSearchButton.TabIndex = 1;
+            this.StockSearchButton.Text = "종목검색";
+            this.StockSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // StockListDataGridView
+            // 
+            this.StockListDataGridView.AllowUserToAddRows = false;
+            this.StockListDataGridView.AllowUserToDeleteRows = false;
+            this.StockListDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StockListDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StockListDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.종목조회_종목코드,
+            this.종목조회_종목명});
+            this.StockListDataGridView.Location = new System.Drawing.Point(12, 81);
+            this.StockListDataGridView.Name = "StockListDataGridView";
+            this.StockListDataGridView.RowHeadersVisible = false;
+            this.StockListDataGridView.RowHeadersWidth = 51;
+            this.StockListDataGridView.RowTemplate.Height = 27;
+            this.StockListDataGridView.Size = new System.Drawing.Size(379, 404);
+            this.StockListDataGridView.TabIndex = 2;
+            this.StockListDataGridView.DoubleClick += new System.EventHandler(this.StockListDataGridView_DoubleClick);
+            // 
+            // 종목조회_종목코드
+            // 
+            this.종목조회_종목코드.HeaderText = "종목코드";
+            this.종목조회_종목코드.MinimumWidth = 6;
+            this.종목조회_종목코드.Name = "종목조회_종목코드";
+            this.종목조회_종목코드.ReadOnly = true;
+            this.종목조회_종목코드.Width = 125;
+            // 
+            // 종목조회_종목명
+            // 
+            this.종목조회_종목명.HeaderText = "종목명";
+            this.종목조회_종목명.MinimumWidth = 6;
+            this.종목조회_종목명.Name = "종목조회_종목명";
+            this.종목조회_종목명.ReadOnly = true;
+            this.종목조회_종목명.Width = 125;
+            // 
+            // StockCheDateDataGridView
+            // 
+            this.StockCheDateDataGridView.AllowUserToAddRows = false;
+            this.StockCheDateDataGridView.AllowUserToDeleteRows = false;
+            this.StockCheDateDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StockCheDateDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StockCheDateDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.che_date_check,
+            this.che_date});
+            this.StockCheDateDataGridView.Location = new System.Drawing.Point(453, 99);
+            this.StockCheDateDataGridView.Name = "StockCheDateDataGridView";
+            this.StockCheDateDataGridView.RowHeadersVisible = false;
+            this.StockCheDateDataGridView.RowHeadersWidth = 51;
+            this.StockCheDateDataGridView.RowTemplate.Height = 27;
+            this.StockCheDateDataGridView.Size = new System.Drawing.Size(314, 352);
+            this.StockCheDateDataGridView.TabIndex = 3;
+            // 
+            // csvOutputButton
+            // 
+            this.csvOutputButton.Location = new System.Drawing.Point(540, 40);
+            this.csvOutputButton.Name = "csvOutputButton";
+            this.csvOutputButton.Size = new System.Drawing.Size(133, 23);
+            this.csvOutputButton.TabIndex = 4;
+            this.csvOutputButton.Text = "CSV로 내보내기";
+            this.csvOutputButton.UseVisualStyleBackColor = true;
+            // 
+            // che_date_check
+            // 
+            this.che_date_check.HeaderText = "선택";
+            this.che_date_check.MinimumWidth = 6;
+            this.che_date_check.Name = "che_date_check";
+            this.che_date_check.Width = 125;
+            // 
+            // che_date
+            // 
+            this.che_date.HeaderText = "체결일자";
+            this.che_date.MinimumWidth = 6;
+            this.che_date.Name = "che_date";
+            this.che_date.ReadOnly = true;
+            this.che_date.Width = 125;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(834, 497);
+            this.Controls.Add(this.csvOutputButton);
+            this.Controls.Add(this.StockCheDateDataGridView);
+            this.Controls.Add(this.StockListDataGridView);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.axKHOpenAPI1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StockListDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StockCheDateDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +193,16 @@
         #endregion
 
         private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox StockSearchTextBox;
+        private System.Windows.Forms.Button StockSearchButton;
+        private System.Windows.Forms.DataGridView StockListDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 종목조회_종목코드;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 종목조회_종목명;
+        private System.Windows.Forms.DataGridView StockCheDateDataGridView;
+        private System.Windows.Forms.Button csvOutputButton;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn che_date_check;
+        private System.Windows.Forms.DataGridViewTextBoxColumn che_date;
     }
 }
 
--- 실시간데이터수집/실시간데이터수집/Form1.cs
+++ 실시간데이터수집/실시간데이터수집/Form1.cs
@@ -10,7 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
 using System.Globalization;
-
+using System.Collections;
+using System.Diagnostics;
 
 namespace 실시간데이터수집
 {
@@ -19,15 +20,61 @@ namespace 실시간데이터수집
         int screenNum = 1000;
         string[] stockCodeList;
         string[] kosdaqCodeList;
+        private SQLiteConnection conn = null;
+        Stopwatch sw = new Stopwatch();
         public Form1()
         {
             InitializeComponent();
 
-            // axKHOpenAPI1.OnEventConnect += API_onEventConnect;
+            axKHOpenAPI1.OnEventConnect += API_onEventConnect;
             // axKHOpenAPI1.OnReceiveRealData += API_onReceiveData;
+            StockSearchButton.Click += stockSearch ;
+            csvOutputButton.Click += csvWriteCheData;
 
-            // axKHOpenAPI1.CommConnect();
-            testInsert();
+            axKHOpenAPI1.CommConnect();
+        }
+
+        private void csvWriteCheData(Object sender, EventArgs e)
+        {
+            List<string> checkedCheDate = new List<string>();
+            foreach(DataGridViewRow row in StockCheDateDataGridView.Rows)
+            {
+                if(row.Cells[0].Value.ToString() == "True")
+                {
+                    Console.WriteLine(row.Cells[1].Value.ToString());
+                    checkedCheDate.Add(row.Cells[1].Value.ToString());
+                }
+            }
+        }
+
+        private void stockSearch(Object sender, EventArgs e) 
+        {
+            StockListDataGridView.Rows.Clear();
+            string searchStockTitle = StockSearchTextBox.Text;
+            Console.WriteLine("검색명 : " + searchStockTitle);
+            if(searchStockTitle.Length == 0)
+            {
+                for (int i = 0; i < stockCodeList.Length - 1; i++)
+                {
+                    StockListDataGridView.Rows.Add();
+                    StockListDataGridView["종목조회_종목코드", i].Value = stockCodeList[i];
+                    StockListDataGridView["종목조회_종목명", i].Value = axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]);
+                }
+            }
+            else
+            {
+                int idx = 0;
+                for (int i = 0; i < stockCodeList.Length - 1; i++)
+                {
+                    if(stockCodeList[i].Contains(searchStockTitle) || axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]).Contains(searchStockTitle))
+                    {
+                        StockListDataGridView.Rows.Add();
+                        StockListDataGridView["종목조회_종목코드", idx].Value = stockCodeList[i];
+                        StockListDataGridView["종목조회_종목명", idx].Value = axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]);
+                        idx++;
+                    }
+                }
+            }
         }
 
         private void API_onEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
@@ -36,7 +83,7 @@ namespace 실시간데이터수집
             {
                 Console.WriteLine("로그인 성공");
                 getStockCode();
-                // dataRequest();
+                // stockReaTimeDataRequest();
             }
             else
             {
@@ -49,9 +96,13 @@ namespace 실시간데이터수집
             if (e.sRealType == "주식체결")
             {
                 // Console.WriteLine("실시간 데이터 : " + e.sRealData);
-                Console.WriteLine("종목 코드 : " + e.sRealKey + ", 현재가 : " + axKHOpenAPI1.GetCommRealData(e.sRealKey, 10) + ", 체결시간 : " + axKHOpenAPI1.GetCommRealData(e.sRealKey, 20));
+                // Console.WriteLine("종목 코드 : " + e.sRealKey + ", 현재가 : " + axKHOpenAPI1.GetCommRealData(e.sRealKey, 10) + ", 체결시간 : " + axKHOpenAPI1.GetCommRealData(e.sRealKey, 20));
                 // DB 삽입
-                realtimeStockDataDBInsert(e.sRealKey, axKHOpenAPI1.GetCommRealData(e.sRealKey, 20), axKHOpenAPI1.GetCommRealData(e.sRealKey, 10), axKHOpenAPI1.GetCommRealData(e.sRealKey, 11), axKHOpenAPI1.GetCommRealData(e.sRealKey, 12), axKHOpenAPI1.GetCommRealData(e.sRealKey, 27), axKHOpenAPI1.GetCommRealData(e.sRealKey, 28), axKHOpenAPI1.GetCommRealData(e.sRealKey, 15), axKHOpenAPI1.GetCommRealData(e.sRealKey, 13));
+                sw.Start();
+                stockRealTimeDataDBInsert(e.sRealKey, datetimePreProcess(axKHOpenAPI1.GetCommRealData(e.sRealKey, 20)), axKHOpenAPI1.GetCommRealData(e.sRealKey, 10), axKHOpenAPI1.GetCommRealData(e.sRealKey, 11), axKHOpenAPI1.GetCommRealData(e.sRealKey, 12), axKHOpenAPI1.GetCommRealData(e.sRealKey, 27), axKHOpenAPI1.GetCommRealData(e.sRealKey, 28), axKHOpenAPI1.GetCommRealData(e.sRealKey, 15), axKHOpenAPI1.GetCommRealData(e.sRealKey, 13));
+                sw.Stop();
+                Console.WriteLine("현재 시간 : " + DateTime.Now + ", Insert 되는 시간 : " + sw.ElapsedMilliseconds.ToString());
+                sw.Reset();
             }
             else
             {
@@ -59,43 +110,9 @@ namespace 실시간데이터수집
             }
         }
 
-        private void testInsert()
-        {
-            // 날짜 변환
-            string dateStr = "144331";
-            int year = DateTime.Now.Year;
-            int month = DateTime.Now.Month;
-            int day = DateTime.Now.Day;
-            int hour = Int32.Parse(dateStr.Substring(0, 2));
-            int minute = Int32.Parse(dateStr.Substring(2, 2));
-            int second = Int32.Parse(dateStr.Substring(4, 2));
-
-            DateTime date = new DateTime(year, month, day, hour, minute, second);
-            Console.WriteLine(date.ToString());
-
-            string result = DateTime.Parse(date.ToString(), new System.Globalization.CultureInfo("ko-KR", true)).ToString("yyyy-MM-dd HH:mm:ss");
-
-            // DateTime result = DateTime.ParseExact(date.ToString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-            Console.WriteLine("result : " + result);
-            Application.Exit();
-
-            
-            dbConnect();
-            string sql = "insert into realtime_che (STOCK_CODE, realtime_datetime, realtime_price, realtime_yesterday, realtime_fluctutation, realtime_sell, realtime_buy, realtime_volume, realtime_cumul_volume) VALUES ('000545', @realtime_datetime, '-16', '-200', '-1.19', '-16700', '-16600', '-100', '131773')";
-
-            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
-            cmd.Parameters.Add("@realtime_datetime", DbType.DateTime);
-            cmd.Prepare();
-
-            cmd.Parameters[0].Value = result;
-            cmd.ExecuteNonQuery();
-            dbDisconnect();
-            
-        }
-
-        private SQLiteConnection conn = null;
         private void dbConnect()
         {
+            Console.WriteLine("DB 연결");
             string strConn = "Data Source=C:\\chichi\\sqliteDB\\mydb.db";
             Console.WriteLine("strConn : " + strConn);
 
@@ -127,14 +144,13 @@ namespace 실시간데이터수집
         }
 
         // REALTIME_CHE 테이블 Insert
-        private void realtimeStockDataDBInsert(string stock_code, string realtime_datetime, string realtime_price, string realtime_yesterday, string realtime_fluctutation, string realtime_sell, string realtime_buy, string realtime_volume, string realtime_cumul_volume)
+        private void stockRealTimeDataDBInsert(string stock_code, string realtime_datetime, string realtime_price, string realtime_yesterday, string realtime_fluctutation, string realtime_sell, string realtime_buy, string realtime_volume, string realtime_cumul_volume)
         {
-            // STOCK_CODE, realtime_datetime, realtime_price, realtime_yesterday, realtime_fluctutation, realtime_sell, realtime_buy, realtime_volume, realtime_cumul_volume
-            string sql = "insert into realtime_che_data (STOCK_CODE, realtime_datetime, realtime_price, realtime_yesterday, realtime_fluctutation, realtime_sell, realtime_buy, realtime_volume, realtime_cumul_volume) VALUES (@STOCK_CODE, @realtime_datetime, @realtime_price, @realtime_yesterday, @realtime_fluctutation, @realtime_sell, @realtime_buy, @realtime_volume, @realtime_cumul_volume)";
+            string sql = "insert into realtime_che (STOCK_CODE, realtime_datetime, realtime_price, realtime_yesterday, realtime_fluctutation, realtime_sell, realtime_buy, realtime_volume, realtime_cumul_volume) VALUES (@STOCK_CODE, @realtime_datetime, @realtime_price, @realtime_yesterday, @realtime_fluctutation, @realtime_sell, @realtime_buy, @realtime_volume, @realtime_cumul_volume)";
 
             SQLiteCommand cmd = new SQLiteCommand(sql, conn);
             cmd.Parameters.Add("@STOCK_CODE", DbType.String);
-            cmd.Parameters.Add("@realtime_datetime", DbType.String);
+            cmd.Parameters.Add("@realtime_datetime", DbType.DateTime);
             cmd.Parameters.Add("@realtime_price", DbType.String);
             cmd.Parameters.Add("@realtime_yesterday", DbType.String);
             cmd.Parameters.Add("@realtime_fluctutation", DbType.String);
@@ -154,15 +170,10 @@ namespace 실시간데이터수집
             cmd.Parameters[7].Value = realtime_volume;
             cmd.Parameters[8].Value = realtime_cumul_volume;
             cmd.ExecuteNonQuery();
-            Console.WriteLine("DB Insert");
         }
 
-        private void dataRequest()
+        private void stockReaTimeDataRequest()
         {
-            // DB연결
-            Console.WriteLine("DB 연결");
-            dbConnect();
-
             // 실시간 연결 요청
             string stockCodeStr = "";
             for(int i=0; i<stockCodeList.Length-1; i++)
@@ -197,25 +208,63 @@ namespace 실시간데이터수집
         // 전종목에 대한 종목코드 가져오기
         private void getStockCode()
         {
+            // 종목코드
             string stockCodeListStr = axKHOpenAPI1.GetCodeListByMarket("0"); // 장내
             string kosdaqCodeListStr = axKHOpenAPI1.GetCodeListByMarket("10"); // 코스닥
             stockCodeList = stockCodeListStr.Split(';');
             kosdaqCodeList = kosdaqCodeListStr.Split(';');
 
-            /*
+            // 디비에서 종목 가져오기 -> 해쉬테이블에 저장
+            Hashtable hashStockCode = new Hashtable();
             dbConnect();
-            for (int i = 0; i < stockCodeList.Length - 1; i++)
+            string sql = "select stock_code, stock_name from stock";
+            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+            SQLiteDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
             {
-                Console.WriteLine("종목 코드 : " + stockCodeList[i] + ", 종목명 : " + axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]));
-                stockDBInesrt(stockCodeList[i], axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]));
+                hashStockCode.Add(rdr["stock_code"], rdr["stock_name"]);
             }
-            for (int i=0; i< kosdaqCodeList.Length-1; i++)
+            rdr.Close();
+
+            // API종목과 디비 종목 비교
+            for(int i=0; i<stockCodeList.Length-1; i++)
             {
-                Console.WriteLine("종목 코드 : " + kosdaqCodeList[i] + ", 종목명 : " + axKHOpenAPI1.GetMasterCodeName(kosdaqCodeList[i]));
-                stockDBInesrt(kosdaqCodeList[i], axKHOpenAPI1.GetMasterCodeName(kosdaqCodeList[i]));
+                if(!hashStockCode.ContainsKey(stockCodeList[i]))
+                {
+                    // 디비에 종목코드가 없다면 insert 해준다.
+                    Console.WriteLine("종목 코드 : " + stockCodeList[i] + ", 종목명 : " + axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]));
+                    stockDBInesrt(stockCodeList[i], axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]));
+                }
+                StockListDataGridView.Rows.Add();
+                StockListDataGridView["종목조회_종목코드", i].Value = stockCodeList[i];
+                StockListDataGridView["종목조회_종목명", i].Value = axKHOpenAPI1.GetMasterCodeName(stockCodeList[i]);
             }
-            dbDisconnect();
-            */
+            for (int i = 0; i < kosdaqCodeList.Length - 1; i++)
+            {
+                if (!hashStockCode.ContainsKey(kosdaqCodeList[i]))
+                {
+                    // 디비에 종목코드가 없다면 insert 해준다.
+                    Console.WriteLine("종목 코드 : " + kosdaqCodeList[i] + ", 종목명 : " + axKHOpenAPI1.GetMasterCodeName(kosdaqCodeList[i]));
+                    stockDBInesrt(kosdaqCodeList[i], axKHOpenAPI1.GetMasterCodeName(kosdaqCodeList[i]));
+                }
+            }
+        }
+
+        // 날짜 변환
+        private string datetimePreProcess(string dateStr)
+        {
+            int year = DateTime.Now.Year;
+            int month = DateTime.Now.Month;
+            int day = DateTime.Now.Day;
+            int hour = Int32.Parse(dateStr.Substring(0, 2));
+            int minute = Int32.Parse(dateStr.Substring(2, 2));
+            int second = Int32.Parse(dateStr.Substring(4, 2));
+
+            DateTime date = new DateTime(year, month, day, hour, minute, second);
+            // Console.WriteLine(date.ToString());
+
+            string result = DateTime.Parse(date.ToString(), new System.Globalization.CultureInfo("ko-KR", true)).ToString("yyyy-MM-dd HH:mm:ss");
+            return result;
         }
 
         private string getScreenNum()
@@ -223,5 +272,36 @@ namespace 실시간데이터수집
             screenNum++;
             return screenNum.ToString();
         }
+
+        private void StockListDataGridView_DoubleClick(object sender, EventArgs e)
+        {
+            string searchStockCode = StockListDataGridView.CurrentRow.Cells["종목조회_종목코드"].Value.ToString();
+            Console.WriteLine("선택된 종목 코드 : " + searchStockCode);
+            List<string> stockCheDateList = stockCheDateSelect(searchStockCode);
+            StockCheDateDataGridView.Rows.Clear();
+            for (int i=0; i<stockCheDateList.Count; i++)
+            {
+                StockCheDateDataGridView.Rows.Add();
+                StockCheDateDataGridView["che_date", i].Value = stockCheDateList[i];
+            }
+        }
+
+        private List<string> stockCheDateSelect(string stock_code)
+        {
+            string sql = "select strftime('%Y-%m-%d', b.realtime_datetime) as che_date from stock as a join realtime_che as b on a.stock_code = b.stock_code where a.stock_code = @STOCK_CODE group by strftime('%Y-%m-%d', b.realtime_datetime);";
+            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+            cmd.Parameters.Add("@STOCK_CODE", DbType.String);
+            cmd.Prepare();
+            cmd.Parameters[0].Value = stock_code;
+            SQLiteDataReader rdr = cmd.ExecuteReader();
+            List<string> stockCheDateList = new List<string>();
+            while (rdr.Read())
+            {
+                stockCheDateList.Add(rdr["che_date"].ToString());
+            }
+            rdr.Close();
+            return stockCheDateList;
+        }
+
     }
 }
